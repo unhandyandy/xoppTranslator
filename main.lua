6613606a5d5face42decea01e4e9422921762420
Andrew Dabrowski <unhandyandy@gmail.com>
2026-07-02 20:36:04 -0400

    prefs file

--- main.lua
+++ main.lua
@@ -1,16 +1,4 @@
-
-local browser="min"
--- local browser="google-chrome"
--- local browser="firefox"
-
--- local screenshotter="maim -s "
-local screenshotcmd="xfce4-screenshooter -r -s "
-
-local clipmanager = "xfce4-clipman"
--- local clipmanager = "clipboard-indicator"
-
-local tmpImage = "/tmp/xournalpp_ocr_temp.png"
-local tmpText = "/tmp/xournalpp_temp.txt"
+dofile("prefs.lua")
 
 -- Register both tools into Xournal++'s interface loop
 function initUi()
